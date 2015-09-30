b504282ef4ee62e8405deaef674c8ac89965e538
Phil DESTROYER OF WORLDS <phil@hhd.com.au>
2015-09-30 16:09:26 +1000

    added twilio API config

--- philio.rb
+++ philio.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
+require 'sinatra/config_file'
+require 'sinatra/reloader'
+require 'twilio-ruby'
+
+config_file 'config_or_whatever.yml'
+
+account_sid = settings.twilio[:account_sid]
+auth_token_totes_sekret = settings.twilio[:auth_token]
 
 get '/' do
-  'Philio for reals'
+  "oh yeah baby"
 end
 
